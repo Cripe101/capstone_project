ec0e1d624cfbff22240937b4336bfee9dcc15877
Cripe101 <mhegryanlimpangog@gmail.com>
2025-11-22 22:10:37 +0800

    Event Box

--- frontend/src/pages/home-page/EventBox.tsx
+++ frontend/src/pages/home-page/EventBox.tsx
@@ -49,6 +49,54 @@ const EventBox = () => {
         </h2>
         <img src="" alt="Image" className="pl-1" />
       </h1>
+      <h1 className="grid grid-cols-[2fr_3fr] h-52 rounded-lg shadow-sm shadow-blue-900/50 group overflow-hidden">
+        <h2 className="relative flex flex-col gap-3 border-r border-r-blue-900/50 px-4 py-2 group-hover:text-white duration-200">
+          <p className="bg-blue-900 w-full h-full bottom-0 -left-60 -z-50 rounded-l-lg absolute group-hover:left-0 duration-200"></p>
+          <p className="font-semibold">00-00-0000</p>
+          <p className="">ACLC Intrams 2025</p>
+          <p className="line-clamp-4">
+            ACLC most commonly refers to AMA Computer Learning Center, a network
+            of private colleges in the Philippines that is part of the AMA
+            Education System. It offers short-term vocational courses and
+            two-year technical/vocational courses, as well as four-year
+            bachelor's degree programs, with a focus on IT-related and
+            business-related fields.{" "}
+          </p>
+        </h2>
+        <img src="" alt="Image" className="pl-1" />
+      </h1>
+      <h1 className="grid grid-cols-[2fr_3fr] h-52 rounded-lg shadow-sm shadow-blue-900/50 group overflow-hidden">
+        <h2 className="relative flex flex-col gap-3 border-r border-r-blue-900/50 px-4 py-2 group-hover:text-white duration-200">
+          <p className="bg-blue-900 w-full h-full bottom-0 -left-60 -z-50 rounded-l-lg absolute group-hover:left-0 duration-200"></p>
+          <p className="font-semibold">00-00-0000</p>
+          <p className="">ACLC Intrams 2025</p>
+          <p className="line-clamp-4">
+            ACLC most commonly refers to AMA Computer Learning Center, a network
+            of private colleges in the Philippines that is part of the AMA
+            Education System. It offers short-term vocational courses and
+            two-year technical/vocational courses, as well as four-year
+            bachelor's degree programs, with a focus on IT-related and
+            business-related fields.{" "}
+          </p>
+        </h2>
+        <img src="" alt="Image" className="pl-1" />
+      </h1>
+      <h1 className="grid grid-cols-[2fr_3fr] h-52 rounded-lg shadow-sm shadow-blue-900/50 group overflow-hidden">
+        <h2 className="relative flex flex-col gap-3 border-r border-r-blue-900/50 px-4 py-2 group-hover:text-white duration-200">
+          <p className="bg-blue-900 w-full h-full bottom-0 -left-60 -z-50 rounded-l-lg absolute group-hover:left-0 duration-200"></p>
+          <p className="font-semibold">00-00-0000</p>
+          <p className="">ACLC Intrams 2025</p>
+          <p className="line-clamp-4">
+            ACLC most commonly refers to AMA Computer Learning Center, a network
+            of private colleges in the Philippines that is part of the AMA
+            Education System. It offers short-term vocational courses and
+            two-year technical/vocational courses, as well as four-year
+            bachelor's degree programs, with a focus on IT-related and
+            business-related fields.{" "}
+          </p>
+        </h2>
+        <img src="" alt="Image" className="pl-1" />
+      </h1>
     </div>
   );
 };
